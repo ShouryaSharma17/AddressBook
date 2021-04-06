c8748f3371d4464d2ef005d67ffaf41690fab3c3
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-06 18:44:47 +0530

    Able to edit the existing contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,50 +1,117 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
+//import static java.lang.System.*;
 
 public class AddressBook {
 
-    static Scanner scanner=new Scanner(System.in);
+    static Scanner detail=new Scanner(System.in);
+
 
     public static  void contacts() {
         //Welcome message
         System.out.println("Welcome to the Address Book!");
 
+        ArrayList<String> firstName=new ArrayList<String>();
+        ArrayList<String> lastName=new ArrayList<String>();
+        ArrayList<String> city=new ArrayList<String>();
+        ArrayList<String> state=new ArrayList<String>();
+        ArrayList<Integer> zipCode=new ArrayList<Integer>();
+        ArrayList<Integer> phoneNumber=new ArrayList<Integer>();
+        ArrayList<String> emailId=new ArrayList<String>();
+
         while (true) {
             //Taking user Input
             System.out.print("Enter the First Name: ");
-            String firstName = scanner.nextLine();
+            firstName.add(detail.next());
 
             System.out.print("Enter the Last Name: ");
-            String secondName = scanner.nextLine();
+            lastName.add(detail.next());
 
             System.out.print("Enter the City Name: ");
-            String city = scanner.nextLine();
+            city.add(detail.next());
 
             System.out.print("Enter the State Name: ");
-            String state = scanner.nextLine();
+            state.add(detail.next());
 
             System.out.print("Enter the email id: ");
-            String emailId = scanner.nextLine();
-
+            emailId.add(detail.next());
 
             System.out.print("Enter the Zip Code: ");
-            int zipCode = scanner.nextInt();
+            zipCode.add(detail.nextInt());
 
             System.out.print("Enter the Phone number: ");
-            int phoneNumber = scanner.nextInt();
+            phoneNumber.add(detail.nextInt());
 
             System.out.println("Do you want to continue adding contacts? Press Y (yes) or N (No)");
-            String option = scanner.next();
+            String option = detail.next();
 
             if (option.equalsIgnoreCase("n")) {
                 System.out.println("The contact details has been successfully added!");
                 //printing contact details
-                System.out.println("First Name: " + firstName + "| Second Name: " + secondName + "| City: " + city + "| State "
+                System.out.println("First Name: " + firstName + "| Second Name: " + lastName + "| City: " + city + "| State "
                         + state + "| Zip Code:" + zipCode + "| Phone Number:" + phoneNumber + "| Email id: " + emailId);
                 break;
+            }
 
             }
+        //code for editing contact details
+        System.out.println("Do you want to edit any contact? Press Y or N ");
+        String editDetail=detail.next();
+        
+        if(editDetail.equalsIgnoreCase("y")){
+            System.out.println("Enter the first name of the contact");
+            String checkName=detail.next();
+
+            //taking new details to replace the older one
+            if (firstName.contains(checkName)) {
+                System.out.println("The name is present");
+                int pos = firstName.indexOf(checkName);
+                
+                System.out.print("Enter the First Name: ");
+                String editFirstName=detail.next();
+
+                System.out.print("Enter the Last Name: ");
+                String editLastName=detail.next();
+
+                System.out.print("Enter the City Name: ");
+                String editCity=detail.next();
+
+                System.out.print("Enter the State Name: ");
+                String editState=detail.next();
+
+                System.out.print("Enter the Email id: ");
+                String editEmailId=detail.next();
+
+                System.out.print("Enter the Zip: ");
+                int editZipCode=detail.nextInt();
+
+                System.out.print("Enter the phone number: ");
+                int editPhoneNumber=detail.nextInt();
+
+                //setting the new details
+                firstName.set(pos,editFirstName);
+                lastName.set(pos,editLastName);
+                city.set(pos,editCity);
+                state.set(pos,editState);
+                emailId.set(pos,editEmailId);
+                zipCode.set(pos, editZipCode);
+                phoneNumber.set(pos,editPhoneNumber);
+
+                //printing edited details
+                System.out.println("The contact detail has been successfully edited"+firstName+lastName+city+state+emailId+zipCode+phoneNumber);
+
+                            }
+
+            else {
+                System.out.println("The name is not present in the Address Book");
+
+            }
+
+            }
+
         }
-    }
+
     //calling method
     public static void main(String args[]) {
         contacts();
